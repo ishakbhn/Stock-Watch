7bf2d3b4f19eeec9aa4dafd9e6f54c2f2fbfc4e2
ishakbhn <ishakbhn@gmail.com>
2019-02-27 07:02:58 +0800

    alert if invalid symbol ticker

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,11 +1,19 @@
 class StocksController < ApplicationController
 
   def search
+    #check if search box is not empty
     if params[:stock].present?
       @stock = Stock.new_from_search(params[:stock])
-      render 'users/portfolio'
+      #if not empty render to path
+      if @stock
+        render 'users/portfolio'
+      #if invalid symbol display alert
+      else
+        flash[:danger] = "You have entered an invalid ticker symbol"
+        redirect_to portfolio_path
+      end
     else
-      flash[:danger] = "Please enter ticker in search box"
+      flash[:danger] = "Please enter ticker symbol in search box"
       redirect_to portfolio_path
     end
   end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,8 +1,13 @@
 class Stock < ApplicationRecord
 
   def self.new_from_search(ticker_symbol)
-    search_stock = StockQuote::Stock.quote(ticker_symbol)
-    new(name: search_stock.company_name, ticker: search_stock.symbol, last_price: search_stock.latest_price)
+    begin
+      search_stock = StockQuote::Stock.quote(ticker_symbol)
+      new(name: search_stock.company_name, ticker: search_stock.symbol, last_price: search_stock.latest_price)
+    # if invalid ticker name return nil
+    rescue Exception => e
+      return nil
+    end
   end
 
 end
