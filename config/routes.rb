16af6c43ddb1f024e6b6d2b68c216792d5a32c25
ishakbhn <ishakbhn@gmail.com>
2019-02-27 13:13:51 +0800

    delete stock from portfolio

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'portfolio', to: 'users#portfolio'
   get 'search_stocks', to: 'stocks#search'
-  #path for only create route
-  resources :user_stocks, only: [:create]
+  #path for create & destroy route
+  resources :user_stocks, only: [:create, :destroy]
 end
